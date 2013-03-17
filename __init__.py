e0ec72d9c75dd2662330211d4b26665f11dcaadf
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-03-18 00:21:07 +0100

    fix package loading

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # encoding=utf-8
 
 from dperrors import DanmicholoParseError
-from parser import *
 from templateeditor2 import TemplateEditor 
 from preprocessor import preprocessToXml
 from maintext import MainText, condition_for_soup
